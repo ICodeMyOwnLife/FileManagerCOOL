ec488e8958e9900bf6dfb50b8264db99b10711e7
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-16 17:45:26 +0700

    Add UpdateNewNames

---
+++ FileManagerWindows/Helpers/FileSystemInfoTemplateSelector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using FileManagerWindows.Models;
+
+
+namespace FileManagerWindows.Helpers
+{
+    public class FileSystemInfoTemplateSelector: DataTemplateSelector
+    {
+        #region Override
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var info = (FileSystemInfo)item;
+            if (info == null) return null;
+
+            var element = (FrameworkElement)container;
+
+            switch (info.Type)
+            {
+                case FileSystemType.Folder:
+                    return element.FindResource("FolderTemplate") as DataTemplate;
+                case FileSystemType.File:
+                    return element.FindResource("FileTemplate") as DataTemplate;
+                case FileSystemType.Compression:
+                    return element.FindResource("CompressionTemplate") as DataTemplate;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        #endregion
+    }
+}
--- FileManagerWindows/ViewModels/MainViewModel.cs
+++ FileManagerWindows/ViewModels/MainViewModel.cs
@@ -16,9 +16,16 @@ namespace FileManagerWindows.ViewModels
     public class MainViewModel: PrismViewModelBase
     {
         #region Fields
+        private const char DEFAULT_MASK_CHAR = '0';
+        private const string DEFAULT_NEW_NAME = "New";
+        private string _baseName = $"{DEFAULT_NEW_NAME}{DEFAULT_MASK_CHAR}{DEFAULT_MASK_CHAR}";
         private bool _canExtract;
         private bool _deleteAfterExtracted;
         private IEnumerable<FileSystemInfo> _entries;
+        private char _maskChar = DEFAULT_MASK_CHAR;
+        private IEnumerable<string> _newNames;
+        private int _startAt = 1;
+        private bool _useMask = true;
         #endregion
 
 
@@ -38,6 +45,12 @@ namespace FileManagerWindows.ViewModels
 
 
         #region  Properties & Indexers
+        public string BaseName
+        {
+            get { return _baseName; }
+            set { SetProperty(ref _baseName, value); }
+        }
+
         public bool CanExtract
         {
             get { return _canExtract; }
@@ -65,8 +78,33 @@ namespace FileManagerWindows.ViewModels
                 {
                     CanExtract = value != null &&
                                  value.Any(f => f.Type == FileSystemType.Compression || f.Type == FileSystemType.Folder);
+                    UpdateNewNames();
                 }
             }
+        }
+
+        public char MaskChar
+        {
+            get { return _maskChar; }
+            set { SetProperty(ref _maskChar, value); }
+        }
+
+        public IEnumerable<string> NewNames
+        {
+            get { return _newNames; }
+            private set { SetProperty(ref _newNames, value); }
+        }
+
+        public int StartAt
+        {
+            get { return _startAt; }
+            set { SetProperty(ref _startAt, value); }
+        }
+
+        public bool UseMask
+        {
+            get { return _useMask; }
+            set { SetProperty(ref _useMask, value); }
         }
         #endregion
 
@@ -110,6 +148,23 @@ namespace FileManagerWindows.ViewModels
         private void ExtractCompresstion(FileSystemInfo compressionEntry)
         {
             // UNDONE: ExtractCompresstion
+        }
+
+        private void UpdateNewNames()
+        {
+            NewNames = Entries?.Select((fsi, i) =>
+            {
+                var baseName = BaseName ?? "";
+                var suffix = (i + StartAt).ToString();
+
+                if (!UseMask) return baseName + suffix;
+
+                var maskCharCount = 0;
+                for (var j = baseName.Length - 1; j >= 0 && baseName[j] == MaskChar; --j, ++maskCharCount) { }
+
+                var prefixLength = baseName.Length - Math.Min(maskCharCount, suffix.Length);
+                return baseName.Substring(0, prefixLength) + suffix;
+            });
         }
         #endregion
     }
